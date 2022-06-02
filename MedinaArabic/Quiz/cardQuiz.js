427a7cd818b46a9b42e9737e16dc81a4061f5c58
khDev <hussaink01@outlook.com>
2022-06-02 13:32:29 +0500

    Module out Menu

--- MedinaArabic/Quiz/cardQuiz.js
+++ MedinaArabic/Quiz/cardQuiz.js
@@ -1,13 +1,16 @@
+import createMenu, { newmin, newmax } from "./quizmenu.js"
 const urlBook1 = "../book1edit.json"
-english = document.getElementById("english")
-arabic = document.getElementById("arabic")
-arabic2 = document.getElementById("arabic2")
-lessonsMenu = document.getElementById("lessonsMenu")
-let result, min, max
+const english = document.getElementById("english")
+const arabic = document.getElementById("arabic")
+const arabic2 = document.getElementById("arabic2")
+const menuContainer = document.getElementById("menuContainer")
+let result,
+  min = 0,
+  max = 100
 let NoOfAnsOptionsToShow = 6
-bookVocabNo = 0
-let questionText, questionValue
-var outputIDs = []
+let bookVocabNo = 0
+let questionText, questonValue
+let outputIDs = []
 const startButton = document.getElementById("start-btn")
 const questionContainerElement = document.getElementById("question-container")
 const BoxquizContainerElement = document.getElementById("boxQuizcontainer")
@@ -16,6 +19,7 @@ const answerButtonsElement = document.getElementById("answer-buttons")
 const iterationElement = document.getElementById("iteration")
 let shuffledQuestions, currentQuestionIndex
 let maxNumberofQuestions = 20
+let book1
 
 // get lesson vocab from json file
 let getVocab = () => {
@@ -24,45 +28,44 @@ let getVocab = () => {
     .then((data) => {
       book1 = data
       bookVocabNo = book1.length
-      noOfLessons = book1[book1.length - 1].L
-      createMenu(noOfLessons)
+      let noOfLessons = book1[book1.length - 1].L
+      createMenu(noOfLessons, book1, setNextQuestion)
     })
     .catch((error) => {
       console.error("Error:", error)
     })
 }
 
-let createMenu = () => {
-  let lsnNo = document.createElement("span")
-  lsnNo.innerText = "Lesson"
-  lessonsMenu.appendChild(lsnNo)
-  for (let lessonNo = 1; lessonNo <= noOfLessons; lessonNo++) {
-    let lsnNo = document.createElement("span")
-    lsnNo.innerHTML = lessonNo
-    lsnNo.classList.add("lessonmenuitem")
-    lsnNo.onclick = function () {
-      filterLesson(lessonNo)
-      // style lesson itmes
-      lessnmenuitems = lessonsMenu.children
-      for (let i = 0; i < lessnmenuitems.length; i++) {
-        let itemmenu = lessnmenuitems[i]
-        itemmenu.style.removeProperty("background-color")
-      }
-      lsnNo.style.backgroundColor = "purple"
-      setNextQuestion()
-    }
-    lessonsMenu.appendChild(lsnNo)
-  }
-}
+// let createMenu = () => {
+//   let menuHeader = document.createElement("span")
+//   menuHeader.innerText = "Lesson"
+//   menuContainer.appendChild(menuHeader)
+//   for (let lessonNo = 1; lessonNo <= noOfLessons; lessonNo++) {
+//     let lsnNo = document.createElement("span")
+//     lsnNo.innerHTML = lessonNo
+//     lsnNo.classList.add("lessonmenuitem")
+//     lsnNo.onclick = function () {
+//       filterLesson(lessonNo)
+//       // style lesson itmes
+//       lessnmenuitems = menuContainer.children
+//       for (let i = 0; i < lessnmenuitems.length; i++) {
+//         let itemmenu = lessnmenuitems[i]
+//         itemmenu.style.removeProperty("background-color")
+//       }
+//       lsnNo.style.backgroundColor = "purple"
+//       setNextQuestion()
+//     }
+//     menuContainer.appendChild(lsnNo)
+//   }
+// }
 
-// return selected lesson vocab id
-let filterLesson = (selectedNo) => {
-  result = book1.filter((obj) => {
-    return obj.L === selectedNo
-  })
-  min = result[0].id
-  max = result.length + min
-}
+// let filterLesson = (selectedNo) => {
+//   result = book1.filter((obj) => {
+//     return obj.L === selectedNo
+//   })
+//   min = result[0].id
+//   max = result.length + min
+// }
 
 const shuffleArr = (array) =>
   array
@@ -72,16 +75,25 @@ const shuffleArr = (array) =>
 
 // Randomise and begin quiz
 let getRndIDs = (min, max) => {
-  vocabAmount = max - min
-  NoOfAnsOptions =
+  let vocabAmount = max - min
+  let NoOfAnsOptions =
     vocabAmount < NoOfAnsOptionsToShow ? vocabAmount : NoOfAnsOptionsToShow
   // outputIDs = []
   while (outputIDs.length < NoOfAnsOptions) {
     var r = Math.floor(Math.random() * (max - min)) + min
     if (outputIDs.indexOf(r) === -1) outputIDs.push(r)
+    // console.log(outputIDs)
   }
 }
 
+// quickly fire through each question
+document.onkeydown = function (evt) {
+  evt = evt || window.event
+  // on z keypress
+  if (evt.keyCode == 90) {
+    setNextQuestion()
+  }
+}
 let startQuiz = () => {
   startButton.classList.add("hide")
   currentQuestionIndex = 0
@@ -92,6 +104,8 @@ let startQuiz = () => {
 startButton.addEventListener("click", startQuiz)
 
 let setNextQuestion = () => {
+  min = newmin() == undefined ? min : newmin()
+  max = newmax() == undefined ? max : newmax()
   currentQuestionIndex++
   resetState()
   getRndIDs(min, max) // start quiz
---
+++ MedinaArabic/Quiz/quizmenu.js
@@ -0,0 +1,43 @@
+// menuContainer = document.getElementById("menuContainer")
+let createMenu = (noOfLessons, book1, setNextQuestion) => {
+  let menuHeader = document.createElement("span")
+  menuHeader.innerText = "Lesson"
+  menuHeader.onclick = function () {
+    // startMegaQuiz()
+  }
+  menuContainer.appendChild(menuHeader)
+  for (let lessonNo = 1; lessonNo <= noOfLessons; lessonNo++) {
+    let lsnNo = document.createElement("span")
+    lsnNo.innerHTML = lessonNo
+    lsnNo.classList.add("lessonmenuitem")
+    lsnNo.onclick = function () {
+      filterLesson(lessonNo, book1)
+      // style lesson itmes
+      let lessnmenuitems = menuContainer.children
+      for (let i = 0; i < lessnmenuitems.length; i++) {
+        let itemmenu = lessnmenuitems[i]
+        itemmenu.style.removeProperty("background-color")
+      }
+      lsnNo.style.backgroundColor = "purple"
+      setNextQuestion()
+    }
+    menuContainer.appendChild(lsnNo)
+  }
+}
+let min, max
+
+// return selected lesson vocab id
+let filterLesson = (selectedNo, book1) => {
+  let result = book1.filter((obj) => {
+    return obj.L === selectedNo
+  })
+  min = result[0].id
+  max = result.length + min
+  console.log("min: " + min + "  Max" + max)
+}
+
+export let newmin = () => min
+export let newmax = () => max
+console.log("min: " + newmin + "  Max:" + newmax)
+
+export default createMenu
